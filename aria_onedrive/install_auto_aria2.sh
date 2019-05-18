10e94dad5a5c3de92eec2226f97771e91678da4c
tkm <mailbox.scmy@gmail.com>
2019-05-18 21:56:27 +0800

    onedrive独立安装以及变量控制源码路径。

--- aria_onedrive/install_auto_aria2.sh
+++ aria_onedrive/install_auto_aria2.sh
@@ -13,6 +13,10 @@ aria2c="/usr/bin/aria2c"
 Crontab_file="/usr/bin/crontab"
 auto_upload="/usr/local/etc/OneDrive"
 
+# 脚本源码路径
+onedrive_script_path="https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business"
+custom_onedrive_script_path="https://67zz.cn/shell"
+
 #fonts color
 Green_font="\033[32m" 
 Red_font="\033[31m" 
@@ -100,7 +104,7 @@ download_aria2(){
 #下载aria2 配置文件
 download_aria2_conf(){
      mkdir "${file}" && cd "${file}"
-	 wget --no-check-certificate -N "https://67zz.cn/shell/aria2.conf"
+	 wget --no-check-certificate -N "${custom_onedrive_script_path}/aria2.conf"
 	 [[ ! -s "aria2.conf" ]] && echo -e "${Error} Aria2 配置文件下载失败 !" && rm -rf "${file}" && exit 1
 	 wget --no-check-certificate -N "https://67zz.cn/Aria2/dht.dat"
 	 [[ ! -s "dht.dat" ]] && echo -e "${Error} Aria2 DHT文件下载失败 !" && rm -rf "${file}" && exit 1
@@ -113,14 +117,14 @@ download_aria2_conf(){
 #下载服务脚本
 service_aria2(){
      if [[ ${release} = "centos" ]]; then
-		if ! wget --no-check-certificate https://67zz.cn/shell/aria2_centos -O /etc/init.d/aria2; then
+		if ! wget --no-check-certificate ${custom_onedrive_script_path}/aria2_centos -O /etc/init.d/aria2; then
 			echo -e "${Error} aria2服务 管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/aria2
 		chkconfig --add aria2
 		chkconfig aria2 on
 	 else
-		if ! wget --no-check-certificate https://67zz.cn/shell/aria2_debian -O /etc/init.d/aria2; then
+		if ! wget --no-check-certificate ${custom_onedrive_script_path}/aria2_debian -O /etc/init.d/aria2; then
 			echo -e "${Error} aria2服务 管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/aria2
@@ -339,26 +343,28 @@ restart_aria2(){
 
 #安装上传脚本
 install_autoUpload(){
-     [[ -e ${auto_upload} ]] && echo -e "${Error} 自动上传脚本已经安装，告辞!" && exit 1
-	 echo -e "${Info}下载安装自动上传脚本..."
-	 mkdir -p ${auto_upload}
-     cd ${auto_upload}
-	 wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/json-parser"
-	 wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive"
-	 wget --no-check-certificate -q -O onedrive-d "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-d"
-	 wget --no-check-certificate -q -O onedrive-authorize "https://67zz.cn/shell/onedrive-authorize"
-	 wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-base"
-	 wget --no-check-certificate -q -O onedrive.cfg "https://67zz.cn/shell/onedrive.cfg"
-	 chmod -R a+x ${auto_upload}
-	 ln -sf ${auto_upload}/onedrive /usr/local/bin/
-	 ln -sf ${auto_upload}/onedrive-d /usr/local/bin/
-     rm -rf $(basename "$0")
-	 wget -P ${file}  --no-check-certificate -N "https://67zz.cn/shell/autoUpload.sh"
-	 sed -i '$a on-download-complete=/root/.aria2/autoUpload.sh' "${aria2_conf}"
-	 chmod 777 ${file}/*
-	 restart_aria2
-	 echo -e "${Green_font}安装完成，复制链接浏览器打开准备授权!"
-	 onedrive -a #去授权
+    [[ -e ${auto_upload} ]] && echo -e "${Error} 自动上传脚本已经安装，告辞!" && exit 1
+	echo -e "${Info}下载安装自动上传脚本..."
+	mkdir -p ${auto_upload}
+    cd ${auto_upload}
+	wget --no-check-certificate -q -O json-parser "${onedrive_script_path}/json-parser"
+	wget --no-check-certificate -q -O onedrive "${onedrive_script_path}/onedrive"
+	wget --no-check-certificate -q -O onedrive-d "${onedrive_script_path}/onedrive-d"
+	wget --no-check-certificate -q -O onedrive-authorize "${custom_onedrive_script_path}/onedrive-authorize"
+	wget --no-check-certificate -q -O onedrive-base "${onedrive_script_path}/onedrive-base"
+	wget --no-check-certificate -q -O onedrive.cfg "${custom_onedrive_script_path}/onedrive.cfg"
+	chmod -R a+x ${auto_upload}
+	ln -sf ${auto_upload}/onedrive /usr/local/bin/
+	ln -sf ${auto_upload}/onedrive-d /usr/local/bin/
+    rm -rf $(basename "$0")
+    if [ -d "${file}" ]; then
+		wget -P ${file}  --no-check-certificate -N "${custom_onedrive_script_path}/autoUpload.sh"
+		sed -i '$a on-download-complete=${file}/autoUpload.sh' "${aria2_conf}"
+		chmod 777 ${file}/*
+		restart_aria2
+	fi
+	echo -e "${Green_font}安装完成，复制链接浏览器打开准备授权!"
+	onedrive -a #去授权
 }
 
 #卸载上传脚本
@@ -372,7 +378,7 @@ uninstall_autoUpload(){
 
 #重新认证
 goto_auth(){
-     [[ ! -e ${auto_upload} ]] && echo -e "${Error} 自动上传脚本未安装,认证个鸡毛啊!" && exit 1
+     [[ ! -e ${auto_upload} ]] && echo -e "${Error} 自动上传脚本未安装!" && exit 1
      onedrive -a #去认证
 }
 
