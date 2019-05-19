645ae69945108bf7d955e94fcd9ebcff8331cde5
tkm <mailbox.scmy@gmail.com>
2019-05-19 14:11:28 +0800

    增加自定义上传目录

--- aria_onedrive/upload.sh
+++ aria_onedrive/upload.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-filename=$1
-echo "uploding $filename"
+if [ $# = 2]; then
+	onedrive_dir=$1
+	filename=$2
+else
+	onedrive_dir=/Downloads
+	filename=$1
+fi
+
+echo "uploding $filename to ${onedrive_dir}"
 
 if [ -d "$filename" ]; then
-    /usr/local/bin/onedrive-d -f /Videos "$filename"
+    /usr/local/bin/onedrive-d -f ${onedrive_dir} "$filename"
 elif [ -f "$filename" ]; then
-   /usr/local/bin/onedrive -f /Videos "$filename"
+   /usr/local/bin/onedrive -f ${onedrive_dir} "$filename"
 else
     echo "filename invalid."
 fi
